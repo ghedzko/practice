05230939375906e0abce7b1f1cf933bda5578938
Guillermo Hedzko <ghedzko@gmail.com>
2023-10-31 20:41:50 -0300

    feat: connect to mongo & add user schema

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User, UserDocument } from './schemas/users.schemma';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return await new this.userModel(createUserDto).save();
+  }
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+}
